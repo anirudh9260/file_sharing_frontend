68efdb6262a431873037895ce852abaf9ba6c2b3
Anirudh <anirudh.agarwal@ril.com>
2023-06-08 15:04:03 +0530

    removed initial state from actinos

--- src/redux/actions/projects.js
+++ src/redux/actions/projects.js
@@ -6,14 +6,22 @@ import {
     fetchProjects,
 } from '../reducer/projects'
 
-const initialState = {
-    projects: [],
-}
 
 export const getProjects = () => async dispatch => {
     await dispatch(fetchProjects())
     try {
         const response = await apiClient.get(PROJECTS_API)
+        console.log("Printing from project actions:", response.data)
+        return dispatch(fetchProjectsSuccess(response.data))
+    } catch (err) {
+        return dispatch(fetchProjectsFailed(err))
+    }
+}
+
+export const addProjects = () => async dispatch => {
+    await dispatch(fetchProjects())
+    try {
+        const response = await apiClient.post(PROJECTS_API)
         console.log("Printing from project actions:", response.data)
         return dispatch(fetchProjectsSuccess(response.data))
     } catch (err) {
