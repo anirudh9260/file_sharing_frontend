2fba4ba071bfe95633a2dbd998f0fac2863c46ab
Anirudh <anirudh.agarwal@ril.com>
2023-06-08 15:02:12 +0530

    file actions

--- src/redux/actions/files.js
+++ src/redux/actions/files.js
@@ -6,16 +6,13 @@ import {
     fetchFiles,
 } from '../reducer/files'
 
-const initialState = {
-    projects: [],
-}
 
-export const getProjects = () => async dispatch => {
+export const getFiles = () => async dispatch => {
     await dispatch(fetchFiles())
     try {
         const response = await apiClient.get(FILES_API)
-        console.log("Printing from project actions:", response.data)
-        return fetchFilesSuccess(response.data)
+        console.log("Printing from files actions:", response.data)
+        return dispatch(fetchFilesSuccess(response.data))
     } catch (err) {
         return dispatch(fetchFilesFailed(err))
     }
