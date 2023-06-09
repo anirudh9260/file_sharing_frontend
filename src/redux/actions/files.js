a7b8f2cfd63bdf175f0b997201a985598d3592b3
Anirudh <anirudh.agarwal@ril.com>
2023-06-09 07:25:45 +0530

    Added create row data function

--- src/redux/actions/files.js
+++ src/redux/actions/files.js
@@ -11,7 +11,7 @@ export const getFiles = () => async dispatch => {
     await dispatch(fetchFiles())
     try {
         const response = await apiClient.get(FILES_API)
-        console.log("Printing from files actions:", response.data)
+        // console.log("Printing from files actions:", response.data)
         return dispatch(fetchFilesSuccess(response.data))
     } catch (err) {
         return dispatch(fetchFilesFailed(err))
--- src/redux/reducer/files.js
+++ src/redux/reducer/files.js
@@ -5,6 +5,17 @@ const initialState = {
     isLoading: false,
 }
 
+function createData(id, file_name, date_modified, type, size, uploaded_by) {
+    return {
+        id,
+        file_name,
+        date_modified,
+        type,
+        size,
+        uploaded_by,
+    }
+}
+
 export const filesReducer = createSlice({
     name: 'files',
     initialState,
@@ -17,11 +28,25 @@ export const filesReducer = createSlice({
         },
         fetchFilesSuccess(state, action) {
             // console.log('Files Reducer data printing', action.payload)
+            let row_data = []
+            for (let i = 0; i < action.payload.length; i++) {
+                row_data.push(
+                    createData(
+                        action.payload[i].id,
+                        action.payload[i].file_name,
+                        action.payload[i].created_at.toString('MMMM yyyy'),
+                        action.payload[i].extension,
+                        action.payload[i].size,
+                        action.payload[i].username,
+                    ),
+                )
+            }
+
             return {
                 ...state,
                 message: 'Files API Successfull',
                 isLoading: false,
-                files: action.payload,
+                files: row_data,
             }
         },
         fetchFilesFailed(state, action) {
