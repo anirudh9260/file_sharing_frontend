d7b826013b530e1abdb13cf065e0874b2180ae8b
Anirudh <anirudh.agarwal@ril.com>
2023-06-09 18:29:24 +0530

    Button changes

--- src/components/RowButton.js
+++ src/components/RowButton.js
@@ -2,14 +2,15 @@ import React from 'react'
 import { Stack, Box, Button, ButtonGroup } from '@mui/material'
 import ShareIcon from '@mui/icons-material/Share'
 import DeleteIcon from '@mui/icons-material/Delete';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
 
 function RowButton() {
     return (
         <Box justifyContent="space-between">
             {/* <ShareIcon></ShareIcon> */}
-            <Button variant="contained" color="success" sx={{mx: 1}}>Download</Button>
-            <Button variant="contained" sx={{ mx: 1}}>Link</Button>
-            <Button variant="text" color="error" sx={{ mx: 1}} startIcon={<DeleteIcon />}>Delete</Button>
+            <Button variant="contained" sx={{mx: 1}} startIcon={<FileDownloadIcon />}>Download</Button>
+            <Button variant="contained" color="success" startIcon={<ShareIcon />} sx={{ mx: 1}}>Copy Link</Button>
+            <Button variant="contained" color="error" sx={{ mx: 1}} startIcon={<DeleteIcon />}>Delete</Button>
         </Box>
     )
 }
