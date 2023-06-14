0d560ee96f5fe115642102e7bcdde9c6440b58eb
Anirudh <anirudh.agarwal@ril.com>
2023-06-14 13:46:19 +0530

    Added project id to get files

--- src/redux/actions/files.js
+++ src/redux/actions/files.js
@@ -7,10 +7,25 @@ import {
 } from '../reducer/files'
 
 
-export const getFiles = () => async dispatch => {
+export const getFiles = (project_id) => async dispatch => {
+    console.log("Project_ID:", project_id)
     await dispatch(fetchFiles())
     try {
         const response = await apiClient.get(FILES_API)
+        // console.log("Printing from files actions:", response.data)
+        return dispatch(fetchFilesSuccess(response.data))
+    } catch (err) {
+        return dispatch(fetchFilesFailed(err))
+    }
+}
+
+
+export const getFilesForProject = (project_id) => async dispatch => {
+    
+    console.log("Project_ID:", project_id)
+    // await dispatch(fetchFiles())
+    try {
+        const response = await apiClient.get(FILES_API, "/", project_id)
         // console.log("Printing from files actions:", response.data)
         return dispatch(fetchFilesSuccess(response.data))
     } catch (err) {
