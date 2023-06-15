f9cdcf74d40d4ab27fd4538b4d3eea1c81ac2220
Anirudh <anirudh.agarwal@ril.com>
2023-06-15 16:22:43 +0530

    Added CRUD operations for projects

--- src/redux/actions/projects.js
+++ src/redux/actions/projects.js
@@ -4,27 +4,67 @@ import {
     fetchProjectsSuccess,
     fetchProjectsFailed,
     fetchProjects,
+    addProjects,
+    addProjectsSuccess,
+    addProjectsFailed,
+    deleteProjects,
+    deleteProjectsSuccess,
+    deleteProjectsFailed,
+    updateProjects,
+    updateProjectsSuccess,
+    updateProjectsFailed
+
 } from '../reducer/projects'
 
-
+// GET Projects Action => GET
 export const getProjects = () => async dispatch => {
     await dispatch(fetchProjects())
     try {
         const response = await apiClient.get(PROJECTS_API)
-        console.log("Printing from project actions:", response.data)
+        console.log('Printing from project actions:', response.data)
         return dispatch(fetchProjectsSuccess(response.data))
     } catch (err) {
         return dispatch(fetchProjectsFailed(err))
     }
 }
 
-export const editProjects = () => async dispatch => {
-    await dispatch(fetchProjects())
+// ADD PROJECTS Action => POST
+export const addProjectAction = body => async dispatch => {
+    console.log('Calling Action : addProjectAction()')
+    await dispatch(addProjects())
     try {
-        const response = await apiClient.post(PROJECTS_API)
-        console.log("Printing from project actions:", response.data)
-        return dispatch(fetchProjectsSuccess(response.data))
+        const response = await apiClient.post(PROJECTS_API, body)
+        console.log('Printing from addProjectAction():', response.data)
+        return dispatch(addProjectsSuccess(response.data))
     } catch (err) {
-        return dispatch(fetchProjectsFailed(err))
+        return dispatch(addProjectsFailed(err))
+    }
+}
+
+// EDIT Projects Action => PUT
+export const editProjectsAction = (projectId, body) => async dispatch => {
+    console.log('Calling Action : editProjectsAction()')
+    await dispatch(updateProjects())
+    try {
+        const response = await apiClient.put(`${PROJECTS_API}/${projectId}`, body)
+        console.log('Printing from editProjectsAction:', response.data)
+        return dispatch(updateProjectsSuccess(response.data))
+    } catch (err) {
+        return dispatch(updateProjectsFailed(err))
+    }
+}
+
+
+
+// DELETE PROJECTS Action => DELETE
+export const deleteProjectAction = projectId => async dispatch => {
+    console.log('Calling Action : deleteProjectAction()')
+    await dispatch(deleteProjects())
+    try {
+        const response = await apiClient.delete(`${PROJECTS_API}/${projectId}`)
+        console.log('Printing from deleteProjectAction():', response.data)
+        return dispatch(deleteProjectsSuccess(response.data))
+    } catch (err) {
+        return dispatch(deleteProjectsFailed(err))
     }
 }
