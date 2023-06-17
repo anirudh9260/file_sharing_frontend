e6093218e4a4b3af9cf61f694bc8d3304b9de784
Anirudh <anirudh.agarwal@ril.com>
2023-06-17 11:22:43 +0530

    Removed unused imports

--- src/components/EnhancedTableContainer.js
+++ src/components/EnhancedTableContainer.js
@@ -1,19 +1,13 @@
 import React from 'react'
 import EnhancedTable from './EnhancedTable'
 
-import { useAppSelector, useAppDispatch } from '../hooks/redux-hooks'
+import { useAppSelector } from '../hooks/redux-hooks'
 
 function EnhancedTableContainer() {
-
-
     const files_data = useAppSelector(state => state.filesReducer)
-
-
     if (files_data.isLoading === false && files_data.files) {
-        console.log("Files Data", files_data.files)
+        return <EnhancedTable rows={files_data.files} />
     }
-
-    return <EnhancedTable rows={files_data.files} />
 }
 
 export default EnhancedTableContainer
