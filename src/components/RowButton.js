58fbe879b14a9aa76c2c86e1d95ca7409f9d12b7
Anirudh <anirudh.agarwal@ril.com>
2023-06-17 09:09:02 +0530

    Added logic in Row Button

--- src/components/RowButton.js
+++ src/components/RowButton.js
@@ -1,16 +1,35 @@
 import React from 'react'
-import { Stack, Box, Button, ButtonGroup } from '@mui/material'
+import { Stack, Box, Button } from '@mui/material'
 import ShareIcon from '@mui/icons-material/Share'
 import DeleteIcon from '@mui/icons-material/Delete';
 import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import { useAppSelector, useAppDispatch } from '../hooks/redux-hooks'
+import { deleteFilesAction } from '../redux/actions/files'
 
-function RowButton() {
+function RowButton(props) {
+    const {row} = props
+
+    const dispatch = useAppDispatch()
+    
+    const handleDownload = () => {
+        console.log(row)
+    }
+
+    const handleCopyLink = () => {
+        console.log(row)
+    }
+
+    const handleDelete = () => {
+        console.log(row)
+        dispatch(deleteFilesAction(row.id))
+    }
+
     return (
         <Box justifyContent="space-between">
             {/* <ShareIcon></ShareIcon> */}
-            <Button variant="contained" sx={{mx: 1}} startIcon={<FileDownloadIcon />}>Download</Button>
-            <Button variant="contained" color="success" startIcon={<ShareIcon />} sx={{ mx: 1}}>Copy Link</Button>
-            <Button variant="contained" color="error" sx={{ mx: 1}} startIcon={<DeleteIcon />}>Delete</Button>
+            <Button variant="contained" sx={{mx: 1}} onClick={handleDownload} startIcon={<FileDownloadIcon />}>Download</Button>
+            <Button variant="contained" color="success" onClick={handleCopyLink} startIcon={<ShareIcon />} sx={{ mx: 1}}>Copy Link</Button>
+            <Button variant="contained" color="error" sx={{ mx: 1}} onClick={handleDelete} startIcon={<DeleteIcon />}>Delete</Button>
         </Box>
     )
 }
