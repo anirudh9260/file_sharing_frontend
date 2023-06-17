2f1e74f1fba18ec647063a00a990dfe20bbf2961
Anirudh <anirudh.agarwal@ril.com>
2023-06-17 11:21:36 +0530

    Added file update, delete error initial values

--- src/redux/reducer/files.js
+++ src/redux/reducer/files.js
@@ -3,7 +3,9 @@ import { createSlice } from '@reduxjs/toolkit'
 const initialState = {
     files: [],
     isLoading: false,
-    isUploading: false
+    isUploading: false,
+    isDeleting: false,
+    isError: false,
 }
 
 function createData(id, file_name, date_modified, type, size, uploaded_by) {
@@ -25,10 +27,10 @@ export const filesReducer = createSlice({
             return {
                 ...state,
                 isLoading: true,
+                isError : false
             }
         },
         fetchFilesSuccess(state, action) {
-            // console.log('Files Reducer data printing', action.payload)
             let row_data = []
             for (let i = 0; i < action.payload.length; i++) {
                 row_data.push(
@@ -45,14 +47,16 @@ export const filesReducer = createSlice({
 
             return {
                 ...state,
-                message: 'Files GET API Successfull',
                 isLoading: false,
                 files: row_data,
+                isError : false
             }
         },
         fetchFilesFailed(state, action) {
             return {
                 ...state,
+                message: 'Files Fetch Failed',
+                isError: true,
                 isLoading: false,
             }
         },
@@ -63,30 +67,59 @@ export const filesReducer = createSlice({
             }
         },
         uploadFilesSuccess(state, action) {
-            console.log("Upload file success")
+            console.log('Upload file success')
             console.log('Files Reducer Upload data', action.payload)
             return {
                 ...state,
-                message: 'Files POST UPLOAD API Successfull',
+                message: 'File Upload Successfull',
                 isUploading: false,
+                isError: false,
             }
         },
         uploadFilesFailed(state, action) {
             return {
                 ...state,
+                message: 'File Upload Failed',
                 isUploading: false,
+                isError: true,
             }
         },
         removeFiles(state, action) {
-            return []
+            return {
+                ...state,
+                isDeleting: true,
+            }
         },
-        updateFiles(state, action) {
-            return []
+        removeFilesSuccess(state, action) {
+            return {
+                ...state,
+                message: 'File Delete Successfull',
+                isDeleting: false,
+                isError: false,
+            }
+        },
+        removeFilesFailed(state, action) {
+            return {
+                ...state,
+                message: 'File Delete Failed',
+                isDeleting: false,
+                isError: true,
+            }
         },
     },
 })
 
-export const { fetchFiles, fetchFilesSuccess, fetchFilesFailed, uploadFiles, uploadFilesSuccess, uploadFilesFailed, removeFiles, updateFiles } =
-    filesReducer.actions
+export const {
+    fetchFiles,
+    fetchFilesSuccess,
+    fetchFilesFailed,
+    uploadFiles,
+    uploadFilesSuccess,
+    uploadFilesFailed,
+    removeFiles,
+    removeFilesSuccess,
+    removeFilesFailed,
+    updateFiles,
+} = filesReducer.actions
 
 export default filesReducer.reducer
