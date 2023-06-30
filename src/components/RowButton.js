55f6f35a02fe5286106c98cfe03204d9ea22458c
Anirudh <anirudh.agarwal@ril.com>
2023-06-30 17:21:23 +0530

    Added Delete Files Confirmation Dialog

--- src/components/RowButton.js
+++ src/components/RowButton.js
@@ -4,11 +4,8 @@ import ShareIcon from '@mui/icons-material/Share'
 import DeleteIcon from '@mui/icons-material/Delete'
 import FileDownloadIcon from '@mui/icons-material/FileDownload'
 import { useAppDispatch } from '../hooks/redux-hooks'
-import {
-    deleteFilesAction,
-    copyLinkAction,
-    getFilesForProject,
-} from '../redux/actions/files'
+import { deleteFilesAction, copyLinkAction } from '../redux/actions/files'
+import DeleteFileConfirmDialog from './DeleteFileConfirmDialog'
 
 function RowButton(props) {
     const { row } = props
@@ -21,20 +18,12 @@ function RowButton(props) {
         '/' +
         row.file_name
 
-    // const handleDownload = () => {
-    //     console.log(row)
-    //     // http://127.0.0.1:5000/files/09a3614028314cbe9aef04865613794a/abc.json
-    // }
-
     const handleCopyLink = () => {
-        // console.log(row)
         dispatch(copyLinkAction(link))
     }
 
     const handleDelete = () => {
-        console.log(row)
         dispatch(deleteFilesAction(row))
-        // dispatch(getFilesForProject())
     }
 
     return (
@@ -59,7 +48,7 @@ function RowButton(props) {
             >
                 Copy Link
             </Button>
-            <Button
+            {/* <Button
                 variant="contained"
                 color="error"
                 sx={{ mx: 1 }}
@@ -67,7 +56,8 @@ function RowButton(props) {
                 startIcon={<DeleteIcon />}
             >
                 Delete
-            </Button>
+            </Button> */}
+            <DeleteFileConfirmDialog row = {row}></DeleteFileConfirmDialog>
         </Box>
     )
 }
