d6fc31e6ba8c79ea53252e3dcd9705dd946f5bbb
Anirudh <anirudh.agarwal@ril.com>
2023-07-14 03:18:02 +0530

    Added users API

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,6 +2,6 @@ export const LOGIN_API = '/auth/login'
 export const REGISTER_API = '/auth/signup'
 export const CHANGE_PASSWORD = '/auth/change_password'
 
-
+export const USERS_API = '/users'
 export const PROJECTS_API = '/projects'
 export const FILES_API = '/files'
