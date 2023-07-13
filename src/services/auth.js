12a2c6b9af9cedba16506c5eb58a9a7078a236af
Anirudh <anirudh.agarwal@ril.com>
2023-07-14 03:08:12 +0530

    user token changes

--- src/services/auth.js
+++ src/services/auth.js
@@ -3,14 +3,19 @@ const getToken = () => {
     return user?.access_token
 }
 
+const setUserToken = token => {
+    let user = JSON.parse(localStorage.getItem('user'))
+    user.access_token = token
+}
+
 const getRefreshToken = () => {
     const user = JSON.parse(localStorage.getItem('user'))
     return user?.refresh_token
 }
 
-const setUserToken = token => {
+const setRefreshToken = token => {
     let user = JSON.parse(localStorage.getItem('user'))
-    user.access_token = token
+    user.refresh_token = token
 }
 
 const getUser = () => {
@@ -18,7 +23,7 @@ const getUser = () => {
 }
 
 const setUser = user => {
-    console.log(JSON.stringify(user))
+    // console.log(JSON.stringify(user))
     localStorage.setItem('user', JSON.stringify(user))
 }
 
@@ -26,18 +31,38 @@ const removeUser = () => {
     localStorage.removeItem('user')
 }
 
+const getUserId = () => {
+    return JSON.parse(localStorage.getItem('user')).userId
+}
+
+const getUserName = () => {
+    return JSON.parse(localStorage.getItem('user')).username
+}
+
+const getUserRole = () => {
+    return JSON.parse(localStorage.getItem('user')).role
+}
+
+const isAdmin = () => {
+    return JSON.parse(localStorage.getItem('user')).role === 'admin'
+}
+
 const isAuthenticated = () => {
     return JSON.parse(localStorage.getItem('user')) !== null
-
 }
 
 const UserSession = {
     getToken,
-    getRefreshToken,
-    getUser,
     setUserToken,
+    getRefreshToken,
+    setRefreshToken,
+    getUser,
     setUser,
     removeUser,
+    getUserId,
+    getUserName,
+    getUserRole,
+    isAdmin,
     isAuthenticated,
 }
 
