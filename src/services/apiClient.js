37aabad4a51e212144bf0c8cbe45bf7fd45908d2
Anirudh <anirudh.agarwal@ril.com>
2023-07-14 03:09:47 +0530

    Removed interceptor

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import UserSession from './auth'
 
 const token = UserSession.getToken()
-// console.log(token)
+
 const apiClient = axios.create({
     baseURL: `${process.env.REACT_APP_API_URL}`,
     headers: {
@@ -11,18 +11,18 @@ const apiClient = axios.create({
     }
 })
 
-apiClient.interceptors.response.use(
-    function (response) {
-        return response
-    },
-    function (error) {
-        let res = error.response
-        if (res.status == 401) {
-            console.log('Something went wrong')
-        }
-        return Promise.reject(error)
-    },
-)
+// apiClient.interceptors.response.use(
+//     function (response) {
+//         return response
+//     },
+//     function (error) {
+//         let res = error.response
+//         if (res.status == 401) {
+//             console.log('Something went wrong')
+//         }
+//         return Promise.reject(error)
+//     },
+// )
 
 export const uploadApiClient = axios.create({
     baseURL: `${process.env.REACT_APP_API_URL}`,
