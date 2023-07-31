7b33764e916a5aa8eec81eb8c6468bafd36c632f
Anirudh Agarwal <anirudh.agarwal@ril.com>
2023-07-31 16:19:07 +0530

    Added Copy link Modal

--- src/components/CopyLinkButton.js
+++ src/components/CopyLinkButton.js
@@ -1,16 +1,37 @@
 import React from 'react'
-import { Box, Button } from '@mui/material'
 
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy'
+import { Box, Button, FormGroup, Stack, Typography } from '@mui/material'
+import Dialog from '@mui/material/Dialog'
+import DialogTitle from '@mui/material/DialogTitle'
+import Modal from '@mui/material/Modal'
+import TextField from '@mui/material/TextField'
 
 import { useAppDispatch } from '../hooks/redux-hooks'
 import { copyLinkAction } from '../redux/actions/files'
 
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    // height: 500,
+    // width: 600,
+    fullWidth: true,
+    fullHeight: true,
+    // maxHeight: "md",
+    // maxWidth: "md",
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+}
 
 export default function CopyLinkButton(props) {
     const { row } = props
 
     const dispatch = useAppDispatch()
+
     const link =
         `${process.env.REACT_APP_API_URL}` +
         '/files/' +
@@ -22,17 +43,67 @@ export default function CopyLinkButton(props) {
         dispatch(copyLinkAction(link))
     }
 
+    const [open, setOpen] = React.useState(false)
+
+    const handleClickOpen = () => {
+        setOpen(true)
+    }
+
+    const handleClose = () => {
+        setOpen(false)
+    }
+
     return (
- 
-            
+        <div>
             <Button
                 variant="contained"
                 color="success"
-                onClick={handleCopyLink}
+                onClick={handleClickOpen}
                 startIcon={<ContentCopyIcon />}
                 sx={{ mx: 1 }}
             >
                 Copy Link
             </Button>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Stack
+                        direction="column"
+                        justifyContent="space-around"
+                        spacing={4}
+                        width={700}
+                    >
+                        <Typography
+                            variant="p"
+                            fontFamily="roboto"
+                            fontWeight={500}
+                            onClick={handleClickOpen}
+                        >
+                            {row.file_name}
+                        </Typography>
+                        <FormGroup>
+                            <TextField
+                                id="standard-name"
+                                label="Link"
+                                value={link}
+                                readOnly={true}
+                                fullWidth
+                                InputProps={{
+                                    endAdornment: (
+                                        <ContentCopyIcon
+                                            onClick={handleCopyLink}
+                                        />
+                                    ),
+                                }}
+                            />
+                        </FormGroup>
+                    </Stack>
+                </Box>
+            </Modal>
+        </div>
     )
 }
