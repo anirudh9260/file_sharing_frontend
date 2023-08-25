862ad4d81d21c580a3e755166a0d1d934a8798f7
Anirudh Agarwal <anirudh.agarwal@ril.com>
2023-08-25 08:16:54 +0530

    converted to modal from dialog box

--- src/components/UploadFile.js
+++ src/components/UploadFile.js
@@ -1,15 +1,33 @@
 import * as React from 'react'
-import Button from '@mui/material/Button'
-import Dialog from '@mui/material/Dialog'
-import DialogActions from '@mui/material/DialogActions'
-import DialogContent from '@mui/material/DialogContent'
-import DialogContentText from '@mui/material/DialogContentText'
-import DialogTitle from '@mui/material/DialogTitle'
-import FileUploadIcon from '@mui/icons-material/FileUpload'
-import PropTypes from 'prop-types'
 import { useState } from 'react'
-import { uploadFilesAction } from '../redux/actions/files'
+
+import FileUploadIcon from '@mui/icons-material/FileUpload'
+import { Button, Box, Stack } from '@mui/material'
+import { Typography } from '@mui/material'
+import Modal from '@mui/material/Modal'
+import PropTypes from 'prop-types'
+import { extensions } from '../constants/conversions'
 import { useAppDispatch, useAppSelector } from '../hooks/redux-hooks'
+import { uploadFilesAction, selectFileAction } from '../redux/actions/files'
+import AllowedExtensions from './AllowedExtensions'
+import { setFilesMessage } from '../redux/reducer/files'
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    // height: 500,
+    // width: 600,
+    fullWidth: true,
+    fullHeight: true,
+    // maxHeight: "md",
+    // maxWidth: "md",
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 2,
+}
 
 function UploadFile() {
     const dispatch = useAppDispatch()
@@ -29,15 +47,26 @@ function UploadFile() {
     }
 
     const handleFileChange = e => {
-        setSelectedFile(e.target.files[0])
-        setIsFilePicked(true)
+        let fileExtension = e.target.files[0].name.split('.').pop()
+        if (extensions.includes(fileExtension)) {
+            setSelectedFile(e.target.files[0])
+            setIsFilePicked(true)
+        } else {
+            setSelectedFile()
+            setIsFilePicked(false)
+            dispatch(setFilesMessage())
+            dispatch(selectFileAction(fileExtension))
+        }
     }
 
     const handleUploadClick = () => {
         if (isFilePicked) {
             const formData = new FormData()
             formData.append('file', selectedFile)
-            formData.append('project_id', projectsState.selectedProject.projectId)
+            formData.append(
+                'project_id',
+                projectsState.selectedProject.projectId,
+            )
             console.log(dispatch(uploadFilesAction(formData)))
             setOpen(false)
         }
@@ -52,30 +81,62 @@ function UploadFile() {
                 onClick={handleClickOpen}
                 sx={{ px: '5', whiteSpace: 'nowrap' }}
             >
-                Upload File <FileUploadIcon></FileUploadIcon>
+                UPLOAD FILE <FileUploadIcon></FileUploadIcon>
             </Button>
-            <Dialog open={open} onClose={handleClose}>
-                <DialogTitle>Upload File</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        <span>
-                            <input type="file" onChange={handleFileChange} />
-                        </span>
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose}>Cancel</Button>
-                    <Button
-                        variant="contained"
-                        size="small"
-                        sx={{ mx: 1 }}
-                        startIcon={<FileUploadIcon />}
-                        onClick={handleUploadClick}
+
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Stack
+                        direction="column"
+                        alignItems="center"
+                        justifyContent="space-around"
+                        spacing={4}
+                        width={350}
                     >
-                        Upload
-                    </Button>
-                </DialogActions>
-            </Dialog>
+                        <Stack
+                            direction="row"
+                            justifyContent="space-evenly"
+                            spacing={15}
+                        >
+                            <Typography variant="h6">
+                                UPLOAD FILE
+                            </Typography>
+                            <AllowedExtensions></AllowedExtensions>
+                        </Stack>
+                        <Box>
+                            <input
+                                id="files"
+                                type="file"
+                                onChange={handleFileChange}
+                            />
+
+</Box>
+                        <Stack
+                            direction="row"
+                            justifyContent="flex-end"
+                            alignItems="center"
+                            spacing={4}
+                        >
+                            <Button onClick={handleClose}>Cancel</Button>
+                            <Button
+                                variant="contained"
+                                size="small"
+                                sx={{ mx: 1 }}
+                                startIcon={<FileUploadIcon />}
+                                onClick={handleUploadClick}
+                                disabled={!isFilePicked}
+                            >
+                                Upload
+                            </Button>
+                        </Stack>
+                    </Stack>
+                </Box>
+            </Modal>
         </div>
     )
 }
